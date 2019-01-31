44f03058e89b789911ff66dbba78dadec266524d
Dr-WaSaBi <russellriker@gmail.com>
2019-01-30 20:25:28 -0500

    Cleaned up code Added a bunch of comments, and tried to clean the code up to make it easier to read.

--- src/main/java/frc/robot/DeadBand.java
+++ src/main/java/frc/robot/DeadBand.java
@@ -1,37 +1,30 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
 package frc.robot;
-
-
-
 /**
- * a class to try to tone down the dead pan in the joysticks to make
- * the robot less jumpy
+ * A class to try to tone down the dead pan in the joysticks to make
+ * the robot less jumpy.
+ * Takes in the axis from a joystick, checks if it's under the deadBandrate
+ * set the axis to 0.0 and return it.
  */
-
-
 public class DeadBand {
-    public static final double deadBandrate = 0.2;
+
+    public static final double deadBandrate = 0.15;
 
     public double SmoothAxis(double joyStickAxis){
         
-        if (Math.abs(joyStickAxis) < deadBandrate) {
+      if (Math.abs(joyStickAxis) < deadBandrate) {
             joyStickAxis = 0.0;
+      }
+      else {
+         if (joyStickAxis>0.0) {
+            joyStickAxis = (joyStickAxis - deadBandrate) / (1.0 - deadBandrate);
          }
          else {
-            if (joyStickAxis>0.0) {
-               joyStickAxis = (joyStickAxis - deadBandrate) / (1.0 - deadBandrate);
-            }
-            else {
                joyStickAxis = (joyStickAxis - -deadBandrate) / (1.0 - deadBandrate);
-            }
-         }
+         }// ******** end if > *******
+      }// ****** end if <
 
-        return joyStickAxis;
-    }
-}
+      return joyStickAxis;
+
+    } // *********************** end of SmoothAxis **********************
+
+}// ************************ end of Class DeadBand *********************
